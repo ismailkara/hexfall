064e4c3bae069d10c079e3d04b63be73d9fa7eae
İsmail Kara <>
2020-09-19 22:00:00 +0300

    binding slots to annchors

--- Assets/_Game/Scripts/Controllers/BoardController.cs
+++ Assets/_Game/Scripts/Controllers/BoardController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
@@ -11,10 +13,11 @@ public class BoardController : MonoBehaviour
     
     
     
-    private Tile[,] board;
-    private ObjectPool slotPool, anchorPool;
-    private Vector2 tileSize;
-    private Vector2 boardOffet;
+    private Tile[,] _board;
+    private List<Anchor> _anchors;
+    private ObjectPool _slotPool, _anchorPool;
+    private Vector2 _tileSize;
+    private Vector2 _boardOffet;
     
 
     private GameConfig currentConfig;
@@ -26,8 +29,8 @@ public class BoardController : MonoBehaviour
 
     void init()
     {
-        slotPool = new ObjectPool(tilePrefab);
-        anchorPool = new ObjectPool(anchorPrefab);
+        _slotPool = new ObjectPool(tilePrefab);
+        _anchorPool = new ObjectPool(anchorPrefab);
         subscribeEvents();
     }
   
@@ -44,35 +47,38 @@ public class BoardController : MonoBehaviour
         calculateCellSize();
         buildBoard();
         buildAnchors();
+        registerSlotsToAnchors();
     }
 
     #endregion
 
     void buildBoard()
     {
-        board = new Tile[currentConfig.boardWidth, currentConfig.boardHeight];
+        _board = new Tile[currentConfig.boardWidth, currentConfig.boardHeight];
 
         for (int i = 0; i < currentConfig.boardWidth; i++)
         {
             for (int j = 0; j < currentConfig.boardHeight; j++)
             {
                 Tile tile = spawnTile(i, j);
-                board[i, j] = tile;
+                _board[i, j] = tile;
             }
         }
     }
 
     void buildAnchors()
     {
-        foreach (var slot in board)
+        _anchors = new List<Anchor>();
+        
+        foreach (var slot in _board)
         {
             bool evenColumn = slot.x % 2 == 0;
             
             bool bottomRow = slot.y == 0;
-            bool topRow = slot.y == board.GetLength(1) - 1;
+            bool topRow = slot.y == _board.GetLength(1) - 1;
             
             bool leftColumn = slot.x == 0;
-            bool rightColumn = slot.x == board.GetLength(0) - 1;
+            bool rightColumn = slot.x == _board.GetLength(0) - 1;
 
             bool bottomAndOddColumn = bottomRow && !evenColumn;
             bool topAndEvenColumn = topRow && evenColumn;
@@ -87,12 +93,14 @@ public class BoardController : MonoBehaviour
             {
                 if (!topAndEvenColumn && !bottomAndOddColumn)
                 {
-                    anchor = anchorPool.get<Anchor>();
+                    anchor = _anchorPool.get<Anchor>();
                     anchor.transform.SetParent(anchorHolder);
                     
-                    anchorPosition = slot.rect.anchoredPosition + (.5f * tileSize.x * Vector2.left);
+                    anchorPosition = slot.rect.anchoredPosition + (.5f * _tileSize.x * Vector2.left);
                     
                     anchor.rect.anchoredPosition = anchorPosition;
+                    
+                    _anchors.Add(anchor);
                 }
                 
             }
@@ -101,13 +109,15 @@ public class BoardController : MonoBehaviour
             {
                 if (notEdge)
                 {
-                    anchor = anchorPool.get<Anchor>();
+                    anchor = _anchorPool.get<Anchor>();
                     anchor.transform.SetParent(anchorHolder);
                     
-                    anchorPosition = slot.rect.anchoredPosition + (.5f * tileSize.x * Vector2.right);
+                    anchorPosition = slot.rect.anchoredPosition + (.5f * _tileSize.x * Vector2.right);
                     
                     anchor.rect.anchoredPosition = anchorPosition;
                     
+                    _anchors.Add(anchor);
+
                 }
 
             }
@@ -115,13 +125,41 @@ public class BoardController : MonoBehaviour
         }
     }
 
+
+    void registerSlotsToAnchors()
+    {
+        float distanceTreshold = _tileSize.x * .55f; // tile ın eninin yarısı altıgenin kosesinin merkeze uzaklıgı kadar. Float esitlemek guvenilir olmadıgı icin .5 degil .55 aldım.
+        distanceTreshold *= distanceTreshold; // daha performanslı oldugu icin magnetude degil sqrtMagnitude karsılastırıyorum
+        List<Tile> temp;
+        foreach (var anchor in _anchors)
+        {
+            temp = new List<Tile>();
+            int index = 0;
+            foreach (var slot in _board)
+            {
+                Vector2 distance = slot.rect.anchoredPosition - anchor.rect.anchoredPosition;
+
+                if (distance.sqrMagnitude < distanceTreshold)
+                {
+                    temp.Add(slot);
+                    index++;
+                    if (index == 3)
+                    {
+                        break;
+                    }
+                }
+            }
+            anchor.addSlots(temp);
+        }
+    }
+
     Tile spawnTile(int i, int j)
     {
-        Tile temp = slotPool.get<Tile>();
+        Tile temp = _slotPool.get<Tile>();
         temp.init(this, currentConfig, i, j);
         temp.transform.SetParent(slotHolder);
         
-        temp.rect.sizeDelta = tileSize;
+        temp.rect.sizeDelta = _tileSize;
 
         temp.rect.anchoredPosition = calculatePosition(i, j);
         
@@ -132,19 +170,19 @@ public class BoardController : MonoBehaviour
     {
         Vector2 position = Vector2.zero;
         
-        position = new Vector2(i * .75f, j) * tileSize;
+        position = new Vector2(i * .75f, j) * _tileSize;
         
-        position += (i % 2) * .5f * tileSize.y * Vector2.down; // tek indexteki stunlarn pozisyonlarını asagıya itiyor
+        position += (i % 2) * .5f * _tileSize.y * Vector2.down; // tek indexteki stunlarn pozisyonlarını asagıya itiyor
         
         
         // buradan sonrakiler board da ortalamak icin, anchor larını  merkezde tutmak istedim o yuzden elle ayarladım 
         // yatay ortalamak
-        position += tileSize.x *  boardOffet.x * .25f * Vector2.left; 
-        position += tileSize.x * .5f * Vector2.right;
+        position += _tileSize.x *  _boardOffet.x * .25f * Vector2.left; 
+        position += _tileSize.x * .5f * Vector2.right;
         
         
-        position += tileSize.y *  boardOffet.y * .5f * Vector2.down; 
-        position += tileSize.y * Vector2.up;
+        position += _tileSize.y *  _boardOffet.y * .5f * Vector2.down; 
+        position += _tileSize.y * Vector2.up;
         
         return position;
     }
@@ -166,7 +204,7 @@ public class BoardController : MonoBehaviour
         }
 
 
-        tileSize = new Vector2(possibleMaximumWidth, possibleMaximumWidth / TileSizeRatio);
+        _tileSize = new Vector2(possibleMaximumWidth, possibleMaximumWidth / TileSizeRatio);
     }
 
     float calculateMaxWidth()
@@ -179,7 +217,7 @@ public class BoardController : MonoBehaviour
         boardWidth += .5f; // sonuncu cıkıntısı
         boardWidth += (currentConfig.boardWidth % 2) * 1.5f; // tek sayıda stun varsa en sona eklenecek uzunluk;
 
-        boardOffet.x = boardWidth;
+        _boardOffet.x = boardWidth;
 
         return 2 * slotHolder.rect.width / boardWidth; // x2 cunku bir kenar 1 birim uzunlugunda olursa altıgeni cevreleyen imajın genisiligi 2 olur
     }
@@ -191,7 +229,7 @@ public class BoardController : MonoBehaviour
 
         float boardHeight = currentConfig.boardHeight + .5f; // .5 sonuncunun cıkıntısı
         
-        boardOffet.y = boardHeight;
+        _boardOffet.y = boardHeight;
         
         float tileHeight = slotHolder.rect.height / boardHeight;
         float tileWidth = tileHeight * TileSizeRatio;
--- Assets/_Game/Scripts/Objects/Anchor.cs
+++ Assets/_Game/Scripts/Objects/Anchor.cs
@@ -1,6 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Anchor : MonoBehaviour
 {
     public RectTransform rect;
+    
+    public Tile[] slots;
+
+    public void addSlots(List<Tile> tiles)
+    {
+        slots = new Tile[3];
+        tiles = arrangeSlots(tiles);
+        for (int i = 0; i < 3; i++)
+        {
+            slots[i] = tiles[i];
+        }
+    }
+
+    //slot ların acısına gore sıralı olması gerekir
+    List<Tile> arrangeSlots(List<Tile> tiles)
+    {
+        List<Tile> result = new List<Tile>();
+        float minAngle = 9999;
+        float maxAngle = -9999;
+
+        Tile max = null;
+        Tile min = null;
+
+        foreach (var slot in tiles)
+        {
+            Vector2 distance = slot.rect.anchoredPosition - rect.anchoredPosition;
+            float angle = Mathf.Atan2(distance.y, distance.x);
+            if (maxAngle < angle)
+            {
+                maxAngle = angle;
+                max = slot;
+            }
+            
+            if (minAngle > angle)
+            {
+                minAngle = angle;
+                min = slot;
+            }
+            
+        }
+
+        tiles.Remove(min);
+        tiles.Remove(max);
+        
+        result.Add(min);
+        result.Add(tiles[0]);
+        result.Add(max);
+
+        return result;
+    }
 }
