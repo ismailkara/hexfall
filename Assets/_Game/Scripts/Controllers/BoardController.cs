fabba383dd9084137f0f8d6d1145875392b48f14
İsmail Kara <>
2020-09-19 17:45:49 +0300

    create tiles with maximum possible tile size

--- Assets/_Game/Scripts/Controllers/BoardController.cs
+++ Assets/_Game/Scripts/Controllers/BoardController.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
     public static BoardController Instance;
 
-    private const float CellSizeRatio = 1.1547005383792515290182975610039149112952035025402537520372046529f; // duzgun altıgenin en/boy oranı, 2/√3
+    private const float TileSizeRatio = 1.1547005383792515290182975610039149112952035025402537520372046529f; // duzgun altıgenin boy/en oranı, 2/√3
     
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private RectTransform boardRect;
     
+    
+    
     private Tile[,] board;
     private ObjectPool pool;
+    private Vector2 tileSize;
     
 
     private GameConfig currentConfig;
@@ -37,6 +39,7 @@ public class BoardController : MonoBehaviour
     void handleNewGame(GameConfig config)
     {
         currentConfig = config;
+        calculateCellSize();
         buildBoard();
     }
 
@@ -58,6 +61,64 @@ public class BoardController : MonoBehaviour
     void spawnTile(int i, int j)
     {
         Tile temp = pool.get<Tile>();
+        temp.transform.SetParent(boardRect);
         
+        temp.rect.sizeDelta = tileSize;
+        temp.rect.anchoredPosition = new Vector2(i * .75f, j) * tileSize;
+        
+        temp.rect.anchoredPosition += (i % 2) * .5f * tileSize.y * Vector2.down; // tek indexteki stunlarn pozisyonlarını asagıya itiyor
+        
+        
+        // buradan sonrakiler board da ortalamak icin, anchor larını  merkezde tutmak istedim o yuzden elle ayarladım 
+        temp.rect.anchoredPosition += boardRect.rect.width * .5f * Vector2.left; // yatay ortalamak
+        temp.rect.anchoredPosition += tileSize.x * .5f * Vector2.right;
+
+
     }
+
+    //board alanına sıgacak en buyuk tile'ı hesaplama
+    void calculateCellSize()
+    {
+        float possibleMaximumWidth = 0;
+        
+        
+        //yatayda kontrol
+        possibleMaximumWidth = calculateMaxWidth();
+        float heightCalculatedWidth = CalculateMaxWidthForHeight();
+
+        if (possibleMaximumWidth > heightCalculatedWidth)
+        {
+            possibleMaximumWidth = heightCalculatedWidth;
+        }
+
+
+        tileSize = new Vector2(possibleMaximumWidth, possibleMaximumWidth / TileSizeRatio);
+    }
+
+    float calculateMaxWidth()
+    {
+        //altıgenin bir kenarı 1 birim olarak alırsak
+
+        float doubleColumnWidth = 3; //her 2 stunun ekledigi genislik
+
+        float boardWidth = Mathf.Floor((float) currentConfig.boardWidth / 2) * doubleColumnWidth;
+        boardWidth += .5f; // sonuncu cıkıntısı
+        boardWidth += (currentConfig.boardWidth % 2) * .75f; // tek sayıda stun varsa en sona eklenecek uzunluk;
+
+        return 2 * boardRect.rect.width / boardWidth; // x2 cunku bir kenar 1 birim uzunlugunda olursa altıgeni cevreleyen imajın genisiligi 2 olur
+    }
+
+    private float CalculateMaxWidthForHeight()
+    {
+        //oyunun oynanabilmesi icin en az iki stun gerekir, 1 stun olma durumunu hesaplamadım o yuzden
+
+
+        float boardHeight = currentConfig.boardHeight + .5f; // sonuncunun cıkıntısı
+        float tileHeight = boardRect.rect.height / boardHeight;
+        float tileWidth = tileHeight * TileSizeRatio;
+
+        return tileWidth;
+    }
+
+
 }
--- Assets/_Game/Scripts/Objects/Tile.cs
+++ Assets/_Game/Scripts/Objects/Tile.cs
@@ -1,18 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public RectTransform rect;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
