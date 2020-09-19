3c9ecbed59033093387e0d70a0bc3f880323ca70
İsmail Kara <>
2020-09-19 16:47:10 +0300

    spawn tile

--- Assets/_Game/Scripts/Controllers/BoardController.cs
+++ Assets/_Game/Scripts/Controllers/BoardController.cs
@@ -5,19 +5,27 @@ public class BoardController : MonoBehaviour
 {
     public static BoardController Instance;
 
+    private const float CellSizeRatio = 1.1547005383792515290182975610039149112952035025402537520372046529f; // duzgun altıgenin en/boy oranı, 2/√3
+    
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private RectTransform boardRect;
-
+    
     private Tile[,] board;
+    private ObjectPool pool;
     
 
     private GameConfig currentConfig;
     private void Awake()
     {
         Instance = this;
-        subscribeEvents();
+        init();
     }
 
+    void init()
+    {
+        pool = new ObjectPool(tilePrefab);
+        subscribeEvents();
+    }
   
 
     void subscribeEvents()
@@ -37,10 +45,19 @@ public class BoardController : MonoBehaviour
     void buildBoard()
     {
         board = new Tile[currentConfig.boardWidth, currentConfig.boardHeight];
+
+        for (int i = 0; i < currentConfig.boardWidth; i++)
+        {
+            for (int j = 0; j < currentConfig.boardHeight; j++)
+            {
+                spawnTile(i, j);
+            }
+        }
     }
 
-    void spawnTile()
+    void spawnTile(int i, int j)
     {
+        Tile temp = pool.get<Tile>();
         
     }
 }
---
+++ Assets/_Game/Scripts/Utilities/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectPool
+{
+    private GameObject template;
+
+    private List<GameObject> pooledObjects;
+    private List<GameObject> usedObjects;
+
+    public  ObjectPool(GameObject temp)
+    {
+        template = temp;
+        pooledObjects = new List<GameObject>();
+        usedObjects = new List<GameObject>();
+    }
+    
+    public T get<T>()
+    {
+        return get().GetComponent<T>();
+    }
+
+    public void recycle(MonoBehaviour obj)
+    {
+        recycle(obj.gameObject);
+    }
+
+    private GameObject get()
+    {
+        GameObject result;
+        if (pooledObjects.Count != 0)
+        {
+            result = pooledObjects[0];
+            pooledObjects.Remove(result);
+        }
+        else
+        {
+            result = GameObject.Instantiate(template);
+        }
+
+        usedObjects.Add(result);
+        return result;
+    }
+
+    private void recycle(GameObject obj)
+    {
+        usedObjects.Remove(obj);
+        pooledObjects.Add(obj);
+    }
+}
