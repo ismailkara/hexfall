4cc9fc4afbbe67268ea9f2e292f37a73ecccd075
İsmail Kara <>
2020-09-22 21:34:48 +0300

    rotate

--- Assets/_Game/Scripts/Controllers/GameLogic.cs
+++ Assets/_Game/Scripts/Controllers/GameLogic.cs
@@ -9,22 +9,54 @@ public class GameLogic : MonoBehaviour
     
     public static Action<List<Tile>> OnTileDestroyed;
 
+    private Anchor _selected;
+
     private void Awake()
     {
         Instance = this;
+
+        InputController.OnDragUp += handleDragUp;
+        InputController.OnDragDown += handleDragDown;
     }
     
+
+    #region event handlers
+
+    void handleDragUp()
+    {
+        Debug.Log("drag up");
+
+        if (_selected != null)
+        {
+            _selected.rotate(1);
+        }
+        
+        
+    }
+
+    void handleDragDown()
+    {
+        Debug.Log("drag down");
+        if (_selected != null)
+        {
+            _selected.rotate(-1);
+        }
+    }
+
+    #endregion
     
     public void anchorSelected(Anchor anchor)
     {
-        List<Tile> temp = new List<Tile>();
-        for (int i = 0; i < anchor.slots.Length; i++)
-        {
-            Slot slot = anchor.slots[i];
-            temp.Add(slot.tile);
-            slot.tile = null;
-        }
-        OnTileDestroyed?.Invoke(temp);
+        _selected = anchor;
+        Debug.Log("anchor selected");
+        // List<Tile> temp = new List<Tile>();
+        // for (int i = 0; i < anchor.slots.Length; i++)
+        // {
+        //     Slot slot = anchor.slots[i];
+        //     temp.Add(slot.tile);
+        //     slot.tile = null;
+        // }
+        // OnTileDestroyed?.Invoke(temp);
     }
 
     public void slotClicked(Slot s)
---
+++ Assets/_Game/Scripts/Controllers/InputController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController Instance;
+
+    public static Action OnDragDown, OnDragUp;
+
+    private const float DistanceTreshold = 10;
+
+    private Vector3 _startPosition;
+    private bool _inputEnabled = true;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+
+    void Update()
+    {
+        if (_inputEnabled)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                onTouchDown();
+            }
+            if (Input.GetMouseButton(0))
+            {
+                onTouchStay();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                onTouchUp();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            OnDragDown?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            OnDragUp?.Invoke();
+        }
+    }
+
+    void onTouchDown()
+    {
+        _startPosition = Input.mousePosition;
+    }
+
+    void onTouchStay()
+    {
+        Vector3 move = Input.mousePosition - _startPosition;
+        move.z = 0;
+        if (Mathf.Abs(move.y) > DistanceTreshold)
+        {
+            if (move.y > 0)
+            {
+                OnDragUp?.Invoke();
+            }
+            else
+            {
+                OnDragDown?.Invoke();
+            }
+        }
+    }
+
+    void onTouchUp()
+    {
+        // Vector3 move = Input.mousePosition - _startPosition;
+        // move.z = 0;
+        // if (move.y > DistanceTreshold)
+        // {
+        //     if (move.y > 0)
+        //     {
+        //         OnDragUp?.Invoke();
+        //     }
+        //     else
+        //     {
+        //         OnDragDown?.Invoke();
+        //     }
+        // }
+    }
+
+    public void disableInput()
+    {
+        _inputEnabled = false;
+    }
+
+    public void enableInput()
+    {
+        _inputEnabled = true;
+        _startPosition = Input.mousePosition;
+    }
+}
