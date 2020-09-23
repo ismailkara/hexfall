644998911f54b8362df566f0425eca764a419b85
İsmail Kara <>
2020-09-23 22:09:45 +0300

    scoreboar

--- Assets/_Game/Scripts/Controllers/ScoreBoard.cs
+++ Assets/_Game/Scripts/Controllers/ScoreBoard.cs
@@ -1,36 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
     private const int Point = 5;
+
+    [SerializeField] private Text scoreText;
+    [SerializeField] private GameObject miniScorePrefab;
+    [SerializeField] private Transform pointHolder;
+
+    private ObjectPool _scorePool;
+
+    private int score;
     void Awake()
     {
+        _scorePool = new ObjectPool(miniScorePrefab);
         subscribeEvents();
     }
 
     void subscribeEvents()
     {
+        GameController.OnNewGame += handleNewGame;
         GameLogic.OnTileDestroyed += handleTilesDestroyed;
     }
 
     #region event handlers
 
+    void handleNewGame(GameConfig config)
+    {
+        score = 0;
+        updateScoreBoard();
+    }
     void handleTilesDestroyed(List<Slot> slots)
     {
         int gainedScore = 0;
         foreach (var slot in slots)
         {
-            
+            if (slot.tile.type == TileType.Starred)
+            {
+                gainedScore += getScore(slot.tile);
+            }
+            else
+            {
+                gainedScore += getScore(slot.tile);;
+            }
+            spawnPoint(slot.tile);
         }
+
+        score += gainedScore;
+        updateScoreBoard();
     }
     
 
     #endregion
-    // Update is called once per frame
-    void Update()
+
+    void updateScoreBoard()
     {
+        scoreText.text = score.ToString();
+    }
+
+    int getScore(Tile tile)
+    {
+        if (tile.type == TileType.Starred)
+        {
+            return 20;
+        }
+
+        return 5;
+    }
+
+    void spawnPoint(Tile tile)
+    {
+        Text point = _scorePool.get<Text>();
+        point.transform.SetParent(pointHolder);
+        point.transform.localScale = Vector3.one;
         
+        point.transform.position = tile.transform.position;
+        
+        point.text = "+" + getScore(tile);
+        
+        Move move = new Move(point.gameObject);
+        move.animTime = .3f;
+        move.moveType = MoveType.WORLD;
+        move.obj.endPosition += Vector3.up * 50;
+        
+        Move shrink = new Move(point.gameObject);
+        shrink.animTime = .2f;
+        shrink.obj.endScale = Vector3.zero;
+        shrink.callback = () =>
+        {
+            _scorePool.recycle(point);
+        };
+
+        move.callback = () =>
+        {
+            shrink.run();
+        };
+        move.run();
     }
 }
---
+++ Assets/_Game/Scripts/Utilities/Dice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Dice
+{
+    private ArrayList _possibilities;
+
+    public Dice()
+    {
+        _possibilities = new ArrayList();
+    }
+
+
+
+    public void add<T>(T a, int weight)
+    {
+        for (int i = 0; i < weight; i++)
+        {
+            _possibilities.Add(a);
+        }
+    }
+
+    public T roll<T>()
+    {
+        int index = Random.Range(0, _possibilities.Count);
+//        Debug.Log(_possibilities.Count);
+        return (T) _possibilities[index];
+    }
+}
+
+
